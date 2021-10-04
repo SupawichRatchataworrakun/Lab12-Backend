a63f1e9aad00b25009bc9fb2eeddcd48855c23ba
SupawichRatchataworrakun <supawich_r@elearning.cmu.ac.th>
2021-10-05 02:13:50 +0700

    7

--- src/main/java/se331/lab/rest/util/LabMapper.java
+++ src/main/java/se331/lab/rest/util/LabMapper.java
@@ -4,6 +4,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import se331.lab.rest.entity.*;
+import se331.lab.rest.security.entity.User;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,6 +14,7 @@ public interface LabMapper {
     LabMapper INSTANCE = Mappers.getMapper(LabMapper.class);
     EventDTO getEventDto(Event event);
     List<EventDTO> getEventDto(List<Event> events);
+    UserDTO getUserDTO(User user);
 
     OrganizerDTO getOrganizerDTO(Organizer organizer);
     List<OrganizerDTO> getOrganizerDTO(List<Organizer> organizers);
